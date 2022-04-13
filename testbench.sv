d1449124463e0ff96a30968ae966953de7b6431e
Assem Ahmad <74145602+azcem@users.noreply.github.com>
2022-04-13 21:03:56 +0200

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,65 +1,31 @@
-//testing up1
-module tb0;
+module tb;
   bit rst, clk, LOSER, WINNER, GAMEOVER, INIT;
+  bit [3:0] winner_score, loser_score;
   who_e WHO;
   bit [1:0] CTRL;
   bit [WIDTH-1:0] val, count;
+  //-------------inistantiating module-----------------------------------
+  game gm(rst, CTRL, val, INIT, clk, LOSER, WINNER, GAMEOVER, winner_score, loser_score, WHO, count);
+  
+  //clk
   always begin
     #1 clk = ~clk;
   end
-  game gm(rst, CTRL, val, INIT, clk, LOSER, WINNER, GAMEOVER, WHO, count);
   
+  initial begin
+    rst = 1;
+    #4 rst = 0;
+    #1 INIT = 1; val = 3;
+    #2 INIT = 0;
+    #232 CTRL = UP_2;
+    #124 CTRL = DOWN_1;
+    #230 CTRL = DOWN_2;
+    
+  end
   initial begin
     $dumpfile("uvm.vcd");
     $dumpvars;
-    #400 $finish;
+    #900 $finish;
   end
-endmodule
 
-//testing down1
-module tb1;
-  bit rst, clk, LOSER, WINNER, GAMEOVER, INIT;
-  who_e WHO;
-  bit [1:0] CTRL;
-  bit [WIDTH-1:0] val, count;
-  always begin
-    #1 clk = ~clk;
-  end
-  
-  initial begin
-    CTRL <= DOWN_1;
-  end
-  
-  game gm(rst, CTRL, val, INIT, clk, LOSER, WINNER, GAMEOVER, WHO, count);
-  
-  initial begin
-    $dumpfile("uvm.vcd");
-    $dumpvars;
-    #400 $finish;
-  end
-endmodule
-
-//testing reset/down1/up2/down2
-module tb3;
-  bit rst, clk, LOSER, WINNER, GAMEOVER, INIT;
-  who_e WHO;
-  bit [1:0] CTRL;
-  bit [WIDTH-1:0] val, count;
-  always begin
-    #1 clk = ~clk;
-  end
-  
-  initial begin
-    CTRL <= UP_2;
-    #4 CTRL <= DOWN_1;
-    #10 rst <= 1;
-    #12 rst <= 0; CTRL <= DOWN_2;
-  end
-  game gm(rst, CTRL, val, INIT, clk, LOSER, WINNER, GAMEOVER, WHO, count);
-  
-  initial begin
-    $dumpfile("uvm.vcd");
-    $dumpvars;
-    #400 $finish;
-  end
 endmodule
